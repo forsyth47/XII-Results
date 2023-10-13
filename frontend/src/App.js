5b247cedcc08ae85d18dc2cf8f51e1e2de880de9
forsyth47 <88034696+forsyth47@users.noreply.github.com>
2023-10-13 16:08:01 +0000

    update2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [files, setFiles] = useState([]);
 
   useEffect(() => {
-    fetch('https://site-files-api.forsyth47.repl.co/api/files')
+    fetch('https://xii-results-backend.vercel.app/api/files')
       .then((response) => response.json())
       .then((data) => setFiles(data.files))
       .catch((error) => console.error('Error fetching files:', error));
@@ -18,7 +18,7 @@ function App() {
         <ul class="file-list" id="fileList">
           {files.map((file, index) => (
             <li key={index}>
-              <a href={`https://site-files-api.forsyth47.repl.co/file/${file}`}>
+              <a href={`https://xii-results-backend.vercel.app/file/${file}`}>
                 <i className="mdi mdi-file"></i>
                 {file}
               </a>

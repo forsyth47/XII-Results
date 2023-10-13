e979edafd6c3df525ce358ee00e6427bec87c30a
forsyth47 <88034696+forsyth47@users.noreply.github.com>
2023-10-13 17:06:04 +0000

    bgf

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
+
 
 const app = express();
 const port = 3001;
@@ -20,16 +22,24 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:filename', (req, res) => {
-    const filename = req.params.filename;
-    const filePath = path.join(directoryPath, filename);
-    console.log(filePath)
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-        return res.status(500).json({ error: 'Error reading file' });
-    }
-    res.send(data);
+  const filename = req.params.filename;
+  const externalUrl = `https://raw.githubusercontent.com/forsyth47/XII-Results/main/backend/sites/${filename}`;
+
+  axios
+    .get(externalUrl)
+    .then(response => {
+      if (response.status === 200) {
+        res.send(response.data);
+      } else {
+        res.status(500).json({ error: 'Error fetching file from external URL' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: 'Error fetching file from external URL' });
     });
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
